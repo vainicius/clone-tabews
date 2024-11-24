786c2b7538cb0ed7e0a3e1d1a11c2815f074f801
vainicius <comercial.viniciusgabriel@gmail.com>
2024-11-24 01:17:08 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabews
+
 Um projeto do curso.dev para me aprofundar em Full-Cycle dev. :)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Volta pra mim, Renata! 💔 </h1>
+  return <h1>Volta pra mim, Renata! 💔 </h1>;
 }
 
+function teste() {
+  console.log("indentação errada");
+}
 export default Home;
